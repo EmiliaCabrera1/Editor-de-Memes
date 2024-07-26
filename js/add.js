e936ba063525afdd41739293badeb0922167a132
EmiliaCabrera1 <emi.22692@gmail.com>
2024-07-25 21:09:19 -0300

    JS Imagen

--- js/add.js
+++ js/add.js
@@ -11,6 +11,30 @@ const darkMode = $("dark-mode");
 const body = $("body");
 const imgHeader = $("img-button-heder-img");
 const textHeader = $("img-button-heder-text");
+const desButton = $("img-button-des");
+const memeBackgroundColor = $("color-background-meme");
+const memeBackgroundEfecto = $("efecto-background-meme");
+const filtroBrillo = $("brillo");
+const filtroOpacidad = $("opacidad");
+const filtroContraste = $("contraste");
+const filtroDesenfoque = $("desenfoque");
+const filtroEscalaDeGrises = $("escalaDeGrises");
+const filtroSepia = $("sepia");
+const filtroHue = $("hue");
+const filtroSaturacion = $("saturacion");
+const filtroNegativo = $("negativo");
+const valoresPredeterminados = {
+  brillo: 1,
+  opacidad: 1,
+  contraste: 100,
+  desenfoque: 0,
+  escalaDeGrises: 0,
+  sepia: 0,
+  hue: 0,
+  saturacion: 100,
+  negativo: 0,
+};
+const reestablecerFiltros = $("reestablecerButton");
 
 textButton.onclick = () => {
   imgSection.classList.add("hidden");
@@ -22,10 +46,6 @@ imgButton.onclick = () => {
   textSection.classList.add("hidden");
 };
 
-urlInput.onchange = (e) => {
-  memeImg.src = e.target.value;
-};
-
 function darkModeFuncion() {
   body.classList.add("dark-mode");
   body.classList.remove("ligth-mode");
@@ -33,6 +53,7 @@ function darkModeFuncion() {
   ligthMode.classList.remove("hidden");
   imgHeader.classList.add("invert-image");
   textHeader.classList.add("invert-image");
+  desButton.classList.add("invert-image");
 }
 darkMode.addEventListener("click", darkModeFuncion);
 
@@ -43,8 +64,64 @@ function ligthModeFuncion() {
   darkMode.classList.remove("hidden");
   imgHeader.classList.remove("invert-image");
   textHeader.classList.remove("invert-image");
+  desButton.classList.remove("invert-image");
 }
 ligthMode.addEventListener("click", ligthModeFuncion);
+
+urlInput.onchange = (e) => {
+  console.log("url(" + e.target.value + ")");
+  memeImg.style.backgroundImage = "url(" + e.target.value + ")";
+  memeImg.style.backgroundSize = "cover";
+  memeImg.style.backgroundRepeat = "no-repeat";
+  memeImg.style.backgroundPosition = "center";
+};
+
+function cambiarBackgroundColor(e) {
+  memeImg.style.backgroundColor = e.target.value;
+}
+memeBackgroundColor.addEventListener("input", cambiarBackgroundColor);
+
+function cambiarBackgroundEfecto(e) {
+  memeImg.style.backgroundBlendMode = e.target.value;
+}
+memeBackgroundEfecto.addEventListener("change", cambiarBackgroundEfecto);
+
+function cambiarFiltro() {
+  memeImg.style.filter = `
+    brightness(${filtroBrillo.value})
+    opacity(${filtroOpacidad.value})
+    contrast(${filtroContraste.value / 100})
+    blur(${filtroDesenfoque.value}px)
+    grayscale(${filtroEscalaDeGrises.value}%)
+    sepia(${filtroSepia.value}%)
+    hue-rotate(${filtroHue.value}deg)
+    saturate(${filtroSaturacion.value}%)
+    invert(${filtroNegativo.value})
+  `;
+}
+filtroBrillo.addEventListener("input", cambiarFiltro);
+filtroOpacidad.addEventListener("input", cambiarFiltro);
+filtroContraste.addEventListener("input", cambiarFiltro);
+filtroDesenfoque.addEventListener("input", cambiarFiltro);
+filtroEscalaDeGrises.addEventListener("input", cambiarFiltro);
+filtroSepia.addEventListener("input", cambiarFiltro);
+filtroHue.addEventListener("input", cambiarFiltro);
+filtroSaturacion.addEventListener("input", cambiarFiltro);
+filtroNegativo.addEventListener("input", cambiarFiltro);
+
+function reestablecer() {
+  filtroBrillo.value = valoresPredeterminados.brillo;
+  filtroOpacidad.value = valoresPredeterminados.opacidad;
+  filtroContraste.value = valoresPredeterminados.contraste;
+  filtroDesenfoque.value = valoresPredeterminados.desenfoque;
+  filtroEscalaDeGrises.value = valoresPredeterminados.escalaDeGrises;
+  filtroSepia.value = valoresPredeterminados.sepia;
+  filtroHue.value = valoresPredeterminados.hue;
+  filtroSaturacion.value = valoresPredeterminados.saturacion;
+  filtroNegativo.value = valoresPredeterminados.negativo;
+  cambiarFiltro();
+}
+reestablecerFiltros.addEventListener("click", reestablecer);
 
 /* DESCARGAR MEME */
 
